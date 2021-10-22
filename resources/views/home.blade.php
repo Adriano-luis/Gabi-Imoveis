1c631a2fe9dd07e316213c8ddf8c2d06bb28cdce
Bruno Henrique Crema <brunohcrema@gmail.com>
2021-10-21 21:06:43 -0300

    estlização dos filtros de imóveis

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,79 +1,86 @@
 @extends('adminlte::page')
 @section('content')
-    <section class="home">
-      <div class="pesquisa">
-        <form action="{{route('home')}}" method="POST">
-            @csrf
-            <label for="bairro">Id Imóvel</label>
-            <input type="text" name="id" class="form-control" id="" placeholder="">
-
-            <label for="bairro">RGI</label>
-            <input type="text" name="rgi" class="form-control" id="" placeholder="">
-
-            <label for="bairro">Valor</label>
-            <input type="text" name="valor" class="form-control" id="" placeholder="">
-
-            <label for="bairro">Endereço</label>
-            <input type="text" name="rua" class="form-control" id="" placeholder="">
-
-            <label for="bairro">Número</label>
-            <input type="text" name="numero" class="form-control" id="" placeholder="">
-
-            <label for="bairro">Metragem</label>
-            <input type="text" name="metragemTot" class="form-control" id="" placeholder="">
-
-            <label for="bairro">Qt. Cômodos</label>
-            <input type="text" name="qtCom" class="form-control" id="" placeholder="">
-
-            <label for="bairro">Qt. Quartos</label>
-            <input type="text" name="qtQuartos" class="form-control" id="" placeholder="">
-
-            <label for="bairro">Qt. Banheiros</label>
-            <input type="text" name="qtBanheiros" class="form-control" id="" placeholder="">
-
-            <label for="bairro">Qt. Vagas de garagem</label>
-            <input type="text" name="qtVagas" class="form-control" id="" placeholder="">
-
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" name="condominioCheck">
-              <label name="condominioCheck" class="form-check-label">Condomínio?</label>
-            </div>
-
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" name="mobiliado">
-              <label name="mobiliado" class="form-check-label">Mobiliado?</label>
-            </div>
-
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" name="pet">
-              <label name="pet" class="form-check-label">Aceita Pet?</label>
-            </div>
-
-            <input type="submit" value="Filtrar">
-        </form>
-    </div>
-      <div class="col-md-3">
-              <div class="card card-success">
-                <div class="card-header">
-                  <h3 class="card-title">Imóvel 1</h3>
-
-                  <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="maximize">
-                      <i class="fas fa-expand"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                      <i class="fas fa-minus"></i>
-                    </button>
-                  </div>
-                  <!-- /.card-tools -->
-                </div>
-                <!-- /.card-header -->
+    
+  <section class="home">
+    <button class="btn btn-ajust btn-primary my-2" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+      Filtros Imóveis
+    </button>
+    <div class="collapse" id="collapseExample">
+      <div class="card card-body position">
+        <div class="pesquisa">
+          <form action="{{route('home')}}" method="POST">
+              @csrf
                 <div class="card-body">
-                  The body of the card
+                    @if (isset($existeImovel))
+                        <div>{{$existeImovel}}</div>
+                    @endif
+                    <div class="form-group col-lg-1">
+                        <label for="idImovel">Id Imóvel</label>
+                        <input type="text" name="idImovel" class="form-control" id="idImovel" placeholder="ID imóvel"  value="{{isset($id) && $id !='' ? $id : '' }}" >
+                    </div>
+                    <div class="row">
+                      <div class="form-group mx-3">
+                          <label for="rgi">RGI</label>
+                          <input type="text" name="rgi" class="form-control" id="rgi" placeholder="RGI">
+                      </div>
+                      <div class="form-group mx-3">
+                          <label for="valor">Valor</label>
+                          <input type="text" name="valor" class="form-control" id="Valor" placeholder="Valor">
+                      </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group mx-3">
+                            <label for="rua">Rua</label>
+                            <input type="text" name="rua" class="form-control" id="rua" placeholder="Rua">
+                        </div>
+                        <div class="form-group mx-3">
+                            <label for="numero">Número</label>
+                            <input type="text" name="numero" class="form-control" id="numero" placeholder="Número">
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="form-group mx-3">
+                            <label for="municipio">Metragem</label>
+                            <input type="text" name="municipio" class="form-control" id="municipio" placeholder="Município">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group mx-3">
+                            <label for="qtCom">Quatidade de comodos</label>
+                            <input type="number" name="qtCom" class="form-control" id="qtCom" placeholder="Quatidade de comodos">
+                        </div>
+                        <div class="form-group mx-3">
+                            <label for="qtQuarto">Quatidade de quarto</label>
+                            <input type="number" name="qtQuarto" class="form-control" id="qtQuarto" placeholder="Quatidade de quartos">
+                        </div>
+                        <div class="form-group mx-3">
+                            <label for="qtBanheiros">Quatidade de banheiro</label>
+                            <input type="number" name="qtBanheiros" class="form-control" id="qtBanheiros" placeholder="Quatidade de banheiro">
+                        </div>
+                        <div class="form-group mx-3">
+                            <label for="qtVagas">Quatidade de vagas de garagem</label>
+                            <input type="number" name="qtVagas" class="form-control" id="qtVagas" placeholder="Qtd de vagas de garagem">
+                        </div>
+                    </div><br>
+                    <div class="row">
+                      <div class="form-check mx-3">
+                        <input class="form-check-input" type="checkbox" name="condominioCheck">
+                        <label name="condominioCheck" class="form-check-label"><h5>Condomínio?<h5></label>
+                      </div><br>
+
+                      <div class="form-check mx-3">
+                        <input class="form-check-input" type="checkbox" name="mobiliado">
+                        <label name="mobiliado" class="form-check-label"><h5>Mobiliado?</h5></label>
+                      </div><br>
+
+                      <div class="form-check mx-3">
+                        <input class="form-check-input" type="checkbox" name="pet">
+                        <label name="pet" class="form-check-label"><h5>Aceita Pet?</h5></label>
+                      </div><br>
+                    </div>
                 </div>
-                <!-- /.card-body -->
-              </div>
-              <!-- /.card -->
+          </form>
+        </div>                
       </div>
-    </section>
+  </section>
 @endsection
--- resources/views/locacao-cadastro-cliente.blade.php
+++ resources/views/locacao-cadastro-cliente.blade.php
@@ -5,114 +5,114 @@
         <form action="{{route('loc-novo-cliente')}}" method="POST">
             @csrf
             <div class="card card-primary">
-            <div class="card-header">
-                <h3 class="card-title">Locator</h3>
-            </div>
-            <div class="card-body">
-                @if (isset($existeImovel))
-                    <div>{{$existeImovel}}</div>
-                @endif
-                <div class="form-group col-lg-3">
-                    <label for="idImovel">Id Imóvel</label>
-                    <input type="text" name="idImovel" class="form-control" id="idImovel"  value="{{isset($id) && $id !='' ? $id : '' }}" 
-                    >
-                </div><br> 
-                <h5>Dados do locator</h5>
-                <div class="form-group">
-                    <label for="nome">Nome</label>
-                    <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome do locator">
+                <div class="card-header">
+                    <h3 class="card-title">Locator</h3>
                 </div>
-                <div class="form-group">
-                    <label for="logradouro">Logradouro</label>
-                    <input type="text" name="logradouro" class="form-control" id="logradouro" placeholder="Endereço do locator">
-                </div>
-                <div class="row">
-                    <div class="form-group mx-1">
-                        <label for="numero">Número</label>
-                        <input type="text" name="numero" class="form-control" id="numero" placeholder="Número">
-                    </div>
-                    <div class="form-group mx-1">
-                        <label for="bairro">Bairro</label>
-                        <input type="text" name="bairro" class="form-control" id="bairro" placeholder="Bairro">
-                    </div>
-                    <div class="form-group mx-1">
-                        <label for="municipio">Município</label>
-                        <input type="text" name="municipio" class="form-control" id="municipio" placeholder="Município">
-                    </div>
-                    <div class="form-group mx-1">
-                        <label for="complemento">Complemento</label>
-                        <input type="text" name="complemento" class="form-control" id="complemento" placeholder="Complemento">
-                    </div>
-                </div>
-                <br>
-                <div class="form-group col-lg-3">
-                        <label>Estado civíl</label>
-                        <select class="custom-select" name="estadoCivil">
-                            <option value="solteiro">Solteiro</option>
-                            <option value="casado">Casado</option>
-                            <option value="separado">Separado </option>
-                            <option value="divorciado">Divorciado </option>
-                            <option value="viuvo">Viúvo</option>
-                        </select>
-                </div>
-                <div class="form-group col-lg-2">
-                    <label for="profissao"> Data de nascimento</label>
-                    <input  type="date" name="nascimento" class="form-control" id="nascimento">
-                </div>
-                <div class="form-group col-lg-2">
-                    <label for="profissao">Profissão</label>
-                    <input  type="text" name="profissao" class="form-control" id="profissao" placeholder="Profissão">
-                </div>
-                <div class="form-group col-lg-2">
-                    <label for="cpf">CPF</label>
-                    <input type="text" name="cpf" class="form-control" id="cpf" placeholder="CPF">
-                </div>
-                <div class="form-group col-lg-2">
-                    <label for="rg">RG</label>
-                    <input type="text" name="rg" class="form-control" id="rg" placeholder="RG">
-                </div>
-                <div class="form-group col-lg-2">
-                    <label for="telefone">Telefone</label>
-                    <input type="phone" name="telefone" class="form-control" id="telefone" placeholder="(dd)x xxxx-xxxx">
-                </div>
-                <br>
-                <h5>Dados de Conta Bancária</h5>
-                <div class="form-group col-lg-2">
-                    <label for="banco">Banco</label>
-                    <input type="text" name="banco" class="form-control" id="banco" placeholder="">
-                </div>
-                <div class="row">
-                    <div class="form-group col-lg-2">
-                        <label for="agencia">Agência</label>
-                        <input type="text" name="agencia" class="form-control" id="agencia" placeholder="Agência">
-                    </div>
+                <div class="card-body">
+                    @if (isset($existeImovel))
+                        <div>{{$existeImovel}}</div>
+                    @endif
                     <div class="form-group col-lg-3">
-                        <label>Tipo de Conta</label>
-                        <select class="custom-select" name="tipoConta">
-                            <option value="corrente">Conta Corrente</option>
-                            <option value="poupanca">Conta Poupança</option>
-                            <option value="salario">Conta Salário</option>
-                            <option value="digital">Conta Digital</option>
-                            <option value="universitaria">Conta Universitária</option>
-                        </select>
+                        <label for="idImovel">Id Imóvel</label>
+                        <input type="text" name="idImovel" class="form-control" id="idImovel"  value="{{isset($id) && $id !='' ? $id : '' }}">
+                    </div><br> 
+                    <h5>Dados do locator</h5>
+                    <div class="form-group">
+                        <label for="nome">Nome</label>
+                        <input type="text" name="nome" class="form-control" id="nome" placeholder="Nome do locator">
                     </div>
-                </div>
-                <div class="row">
+                    <div class="form-group">
+                        <label for="logradouro">Logradouro</label>
+                        <input type="text" name="logradouro" class="form-control" id="logradouro" placeholder="Endereço do locator">
+                    </div>
+                    <div class="row">
+                        <div class="form-group mx-1">
+                            <label for="numero">Número</label>
+                            <input type="text" name="numero" class="form-control" id="numero" placeholder="Número">
+                        </div>
+                        <div class="form-group mx-1">
+                            <label for="bairro">Bairro</label>
+                            <input type="text" name="bairro" class="form-control" id="bairro" placeholder="Bairro">
+                        </div>
+                        <div class="form-group mx-1">
+                            <label for="municipio">Município</label>
+                            <input type="text" name="municipio" class="form-control" id="municipio" placeholder="Município">
+                        </div>
+                        <div class="form-group mx-1">
+                            <label for="complemento">Complemento</label>
+                            <input type="text" name="complemento" class="form-control" id="complemento" placeholder="Complemento">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group col-lg-3">
+                            <label>Estado civíl</label>
+                            <select class="custom-select" name="estadoCivil">
+                                <option value="solteiro">Solteiro</option>
+                                <option value="casado">Casado</option>
+                                <option value="separado">Separado </option>
+                                <option value="divorciado">Divorciado </option>
+                                <option value="viuvo">Viúvo</option>
+                            </select>
+                    </div>
                     <div class="form-group col-lg-2">
-                        <label for="conta">Conta</label>
-                        <input type="text" name="conta" class="form-control" id="conta" placeholder="Conta">
+                        <label for="profissao"> Data de nascimento</label>
+                        <input  type="date" name="nascimento" class="form-control" id="nascimento">
                     </div>
                     <div class="form-group col-lg-2">
-                        <label for="pix">PIX</label>
-                        <input type="text" name="pix" class="form-control" id="pix" placeholder="PIX">
+                        <label for="profissao">Profissão</label>
+                        <input  type="text" name="profissao" class="form-control" id="profissao" placeholder="Profissão">
                     </div>
-                </div>
-                <br>
-                <div class="row">
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary">Salvar</button>
+                    <div class="form-group col-lg-2">
+                        <label for="cpf">CPF</label>
+                        <input type="text" name="cpf" class="form-control" id="cpf" placeholder="CPF">
                     </div>
+                    <div class="form-group col-lg-2">
+                        <label for="rg">RG</label>
+                        <input type="text" name="rg" class="form-control" id="rg" placeholder="RG">
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label for="telefone">Telefone</label>
+                        <input type="phone" name="telefone" class="form-control" id="telefone" placeholder="(dd)x xxxx-xxxx">
+                    </div>
+                    <br>
+                    <h5>Dados de Conta Bancária</h5>
+                    <div class="form-group col-lg-2">
+                        <label for="banco">Banco</label>
+                        <input type="text" name="banco" class="form-control" id="banco" placeholder="">
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-lg-2">
+                            <label for="agencia">Agência</label>
+                            <input type="text" name="agencia" class="form-control" id="agencia" placeholder="Agência">
+                        </div>
+                        <div class="form-group col-lg-3">
+                            <label>Tipo de Conta</label>
+                            <select class="custom-select" name="tipoConta">
+                                <option value="corrente">Conta Corrente</option>
+                                <option value="poupanca">Conta Poupança</option>
+                                <option value="salario">Conta Salário</option>
+                                <option value="digital">Conta Digital</option>
+                                <option value="universitaria">Conta Universitária</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-lg-2">
+                            <label for="conta">Conta</label>
+                            <input type="text" name="conta" class="form-control" id="conta" placeholder="Conta">
+                        </div>
+                        <div class="form-group col-lg-2">
+                            <label for="pix">PIX</label>
+                            <input type="text" name="pix" class="form-control" id="pix" placeholder="PIX">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Salvar</button>
+                        </div>
 
+                    </div>
                 </div>
             </div>
         </form>
