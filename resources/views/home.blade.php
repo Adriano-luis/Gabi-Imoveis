79bb48f1d157caf18079e91919c0737853299ddf
Bruno Henrique Crema <brunohcrema@gmail.com>
2021-10-22 14:09:33 -0300

    card dos imóveis criado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -105,6 +105,163 @@
       </div>
     </div>
 
+    <!-- final dos filtros -->
+
+    <!-- inicio dos cards -->
+
+    <div class="row my-3">
+          <div class="col-lg-4 col-md-4 col-sm-6">
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Primary</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                </div>
+                <!-- /.card-tools -->
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                content
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+          <div class="col-lg-4 col-md-4 col-sm-6">
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Success</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i>
+                  </button>
+                </div>
+                <!-- /.card-tools -->
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                The body of the card
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+          <div class="col-lg-4 col-md-4 col-sm-6">
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Warning</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                </div>
+                <!-- /.card-tools -->
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                The body of the card
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+
+        ou
+        <br>
+
+        <div class="col-md-6">
+            <!-- Box Comment -->
+            <div class="card card-widget">
+              <div class="card-header">
+                <div class="user-block">
+                  <span class="username"><a href="#">Nome do dono</a></span>
+                  <span class="description">Av. Sete de Setembro 3302 </span>
+                  <span class="description">Apelido do imóvel </span>
+                  <span class="description">Imóvel cadastrado dia 22/10/2021 - 10:56 </span>
+                </div>
+                <!-- /.user-block -->
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+                <!-- /.card-tools -->
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="{{URL::asset('../../assets/images/sala3.jpg')}}" alt="comodos">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="{{URL::asset('../../assets/images/quarto.jpg')}}" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="{{URL::asset('../../assets/images/banheiro.jpg')}}" alt="Third slide">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+                
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer card-comments">
+                <div class="card-comment d-flex">
+                  <!-- User image -->
+                  <i class="fas fa-ruler-combined pt-2"></i>
+
+                  <span class="username px-3">
+                      Quantidade de Comodos
+                  </span>
+                    <!-- /.username -->
+                </div><br>
+                <!-- /.card-comment -->
+                <div class="d-flex">
+                  <!-- User image -->
+                  <i class="fas fa-toilet"></i>
+                  <div class="px-3">
+                    Quantidade de banheiros
+                  </div>
+                </div><br>
+                <div class="d-flex">
+                  <!-- User image -->
+                  <i class="fas fa-bed"></i>
+                  <div class="px-3">
+                    Quantidade de Quartos
+                  </div>
+                </div><br>
+                <div class="d-flex">
+                  <!-- User image -->
+                  <i class="fas fa-car"></i>
+                  <div class="px-3">
+                    Quantidade de Garagem
+                  </div>
+                </div>
+                <!-- /.card-comment -->
+              </div>
+            </div>
+            <!-- /.card -->
+    </div>
+
     @if (isset($cliente))
         {{$cliente->nome}}
     @endif
