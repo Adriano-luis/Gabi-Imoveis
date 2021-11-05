1bca3a66ad5721c8dd935851c58f2a9b9ab0c9e0
Bruno Henrique Crema <brunohcrema@gmail.com>
2021-11-05 09:10:46 -0300

    criação das paginas de impressão

---
+++ resources/views/imprimir-proprietario.blade.php
@@ -0,0 +1,7 @@
+    <section class="imprimir">
+        <form action="{{route('imprimir-proprietario')}}">
+            <div class="docImprimir"></div>
+            {{-- conteúdo de impressão --}}
+            <button type="submit" class="btn btn-primary imovel-btn-print">Imprimir</button>
+        </form>
+    </section>
