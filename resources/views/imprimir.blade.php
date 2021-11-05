644c64858477beca3f0a0f1327ffbed6592e9b30
Bruno Henrique Crema <brunohcrema@gmail.com>
2021-11-05 09:10:08 -0300

    criação da pagina de locação

--- resources/views/imprimir.blade.php
+++
@@ -1,7 +0,0 @@
-    <section class="imprimir">
-        <form action="{{route('imprimir')}}">
-            <div class="docImprimir"></div>
-            {{-- conteúdo de impressão --}}
-            <button type="submit" class="btn btn-primary imovel-btn-print">Imprimir</button>
-        </form>
-    </section>
