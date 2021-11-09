9fe8b3269deceb7bccaf1cbaaa3fb824248ffdf5
Bruno Henrique Crema <brunohcrema@gmail.com>
2021-11-09 15:26:48 -0300

    nem sei o que eu dei push

--- resources/views/imovel.blade.php
+++ resources/views/imovel.blade.php
@@ -416,15 +416,9 @@
                                 </div>
                             </div>
                             <div class="row float-right">
-<<<<<<< HEAD
                                 <button type="submit" class="btn btn-primary imovel-btn-print" onClick="window.print()">Imprimir</button>
-                                <button type="submit" class="btn btn-primary imovel-btn-print mx-2">Editar</button>
-                                <button type="submit" class="btn btn-primary imovel-btn-print">Proprietário</button>
-=======
-                                <button type="submit" class="btn btn-primary imovel-btn-print ">Imprimir</button>
-                                <a href="{{route('ven-editar-imovel',['venId'=>$dadosImovel->id])}}" type="submit" class="btn btn-primary imovel-btn-print mx-2">Editar</a>
-                                <a href="{{route('ven-editar-cliente',['id'=>$dadosImovel->idVendedor])}}" type="submit" class="btn btn-primary imovel-btn-print">Proprietário</a>
->>>>>>> c1ea8a0584c60384354750da498bf78cfa39201d
+                                <a href="{{route('loc-editar-imovel', ['idLoc'=>$dadosImovel->id])}}"  class="btn btn-primary imovel-btn-print px-2">Editar</a>
+                                <a href="{{route('loc-editar-cliente',['id'=>$dadosImovel->idLocador])}}" class="btn btn-primary imovel-btn-print">Proprietário</a>
                             </div>
                         </div>
 
