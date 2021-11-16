4a92f619da50491237680937f7830271d6af3e50
Adriano Oliveira <adrianooliveira2332@gmail.com>
2021-11-16 17:59:06 -0300

    collapsse de imoveis parados feito

--- resources/views/imoveis-parados.blade.php
+++ resources/views/imoveis-parados.blade.php
@@ -2,46 +2,60 @@
 @section('content')
     <section class="parados">
         <h1>Esses Imóveis estão parados:</h1>
-        <table border="1" class="table table-striped"  style="text-align:center; font-size:12px">
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Valor</th>
-                    <th>Bairro</th>
-                    <th>Município</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($paradosVend as $parado)
+        <a class="btn btn-primary" data-toggle="collapse" href="#tabelaLoc" role="button" aria-expanded="false" aria-controls="collapse">
+            Locação
+        </a><br/><br/>
+        <div class="collapse" id="tabelaLoc">
+        <div class="card card-body">
+            <table border="1" class="table table-striped"  style="text-align:center; font-size:12px">
+                <thead>
                     <tr>
-                        <td>{{$parado->id}}</td>
-                        <td>{{$parado->valor}}</td>
-                        <td>{{$parado->bairro}}</td>
-                        <td>{{$parado->municipio}}</td>
+                        <th>Id</th>
+                        <th>Valor</th>
+                        <th>Bairro</th>
+                        <th>Município</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($paradosVend as $parado)
+                        <tr>
+                            <td>{{$parado->id}}</td>
+                            <td>{{$parado->valor}}</td>
+                            <td>{{$parado->bairro}}</td>
+                            <td>{{$parado->municipio}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        </div>
 
-        <table border="1" class="table table-striped"  style="text-align:center; font-size:12px">
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Valor</th>
-                    <th>Bairro</th>
-                    <th>Município</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($paradosLoc as $parado)
-                    <tr>
-                        <td>{{$parado->id}}</td>
-                        <td>{{$parado->valor}}</td>
-                        <td>{{$parado->bairro}}</td>
-                        <td>{{$parado->municipio}}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <a class="btn btn-primary" data-toggle="collapse" href="#collapseVen" role="button" aria-expanded="false" aria-controls="collapse">
+            Vendas
+        </a>
+        <div class="collapse" id="collapseVen">
+            <div class="card card-body">
+                <table border="1" class="table table-striped"  style="text-align:center; font-size:12px">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Valor</th>
+                            <th>Bairro</th>
+                            <th>Município</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($paradosLoc as $parado)
+                            <tr>
+                                <td>{{$parado->id}}</td>
+                                <td>{{$parado->valor}}</td>
+                                <td>{{$parado->bairro}}</td>
+                                <td>{{$parado->municipio}}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </section>
 @endsection
